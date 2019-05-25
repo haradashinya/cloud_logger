5fa425f95d5bf983ab5e9e9dd01ade30401db84f
haradashinya <haradashinya@gmail.com>
2019-05-25 20:17:57 +0900

    Fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
         name="cloud-logger",
-        version="0.1.2",
+        version="0.1.3",
         author="furodrive",
         author_email="furodrive@gmail.com",
         packages=setuptools.find_packages(),
